0edb0dd0adc13db8d3dd69e5a861487ed1713007
SebaRenner <sebastian.renner@gmx.ch>
2025-01-03 14:30:59 +0100

    load image as bitmap and daltonize pixel by pixel

--- Daltonization.Console/Program.cs
+++ Daltonization.Console/Program.cs
@@ -1,34 +1,48 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
 
 using Daltonization.Core;
 
-// Read image as bitmap and iterate over each pixel
+var filepath = "../../../Ishihara.jpg";
+var bitmap = new Bitmap(filepath);
 
-var color = Color.FromArgb(255, 200, 100);
+var daltonizedImage = new Bitmap(bitmap.Width, bitmap.Height);
 
-// Step 1: Convert RGB to LMS
-var lms = LMS.FromColor(color);
-
-// Step 2: Simulate CVD
-var lmsSim = Simulator.SimulateColorBlindness(lms.Value, ColorBlindnessType.Protanopia);
-
-// Step 3: Compute Error
-var error = new double[lmsSim.Length];
-
-for (int i = 0; i < error.Length; i++)
+for (int y = 0; y < bitmap.Height; y++)
 {
-    error[i] = lms.Value[i] - lmsSim[i];   
+    for (int x = 0; x < bitmap.Width; x++)
+    {
+        Color pixel = bitmap.GetPixel(x, y);
+
+        // Step 1: Convert RGB to LMS
+        var lms = LMS.FromColor(pixel);
+
+        // Step 2: Simulate CVD
+        var lmsSim = Simulator.SimulateColorBlindness(lms.Value, ColorBlindnessType.Deuteranopia);
+
+        // Step 3: Compute Error
+        var error = new double[lmsSim.Length];
+
+        for (int i = 0; i < error.Length; i++)
+        {
+            error[i] = lms.Value[i] - lmsSim[i];
+        }
+
+        // Step 4: Apply Correction
+        var correctedLms = new double[lmsSim.Length];
+
+        for (int i = 0; i < error.Length; i++)
+        {
+            correctedLms[i] = lmsSim[i] - error[i];
+        }
+
+        // Step 5: Convert back to RGB
+        var correctedRgb = LMS.FromDoubleArr(correctedLms).ToRGB();
+
+        daltonizedImage.SetPixel(x, y, correctedRgb);
+    }
 }
 
-// Step 4: Apply Correction
-var correctedLms = new double[lmsSim.Length];
+daltonizedImage.Save("daltonized_image.jpg", ImageFormat.Jpeg);
 
-for (int i = 0; i < error.Length; i++)
-{
-    correctedLms[i] = lmsSim[i] - error[i];
-}
-
-// Step 5: Convert back to RGB
-var correctedRgb = LMS.FromDoubleArr(correctedLms).ToRGB();
-
-Console.WriteLine(correctedRgb);
+Console.WriteLine("Daltonization finished");
